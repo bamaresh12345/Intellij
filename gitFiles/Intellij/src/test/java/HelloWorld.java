ad80257ae8654fe197cf4a66cf7c5dee5acd4bb3
bamaresh12345 <59543502+bamaresh12345@users.noreply.github.com>
2025-06-21 13:54:45 -0400

    Update HelloWorld.java

--- gitFiles/Intellij/src/test/java/HelloWorld.java
+++ gitFiles/Intellij/src/test/java/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld {
 
     }
 
-    public static void conflitteTest() {
+    public static void Masterchagnes() {
         System.out.println("test This is a message from HelloWorld class.");
     }
 }
