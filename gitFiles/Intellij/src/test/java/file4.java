de2f83701ee8c565773bc1defecaa2fca93950b1
bamaresh12345 <bamaresh2012@gmail.com>
2025-06-21 13:05:43 -0400

    file 3 and 4 added

--- gitFiles/Intellij/src/test/java/file4.java
+++ gitFiles/Intellij/src/test/java/file4.java
@@ -1,6 +1,14 @@
 public class file4 {
 
     public static void main(String[] args) {
+        System.out.println("in file4.java");
 
+        // This is for testing purposes
+        // You can add more functionality here if needed
+        // For example, you could implement a simple method or class to demonstrate functionality
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("This is line " + (i + 1));
+        }
     }
 }
