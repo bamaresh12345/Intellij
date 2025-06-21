36584236d953cd3f9b11aba736160e302f1448b0
bamaresh12345 <bamaresh2012@gmail.com>
2025-06-21 13:09:18 -0400

    file 2 and 1 added

--- gitFiles/Intellij/src/test/java/newfile2.java
+++ gitFiles/Intellij/src/test/java/newfile2.java
@@ -2,5 +2,9 @@ public class newfile2 {
     public static void main(String[] args) {
         System.out.println("in newfile2.java");
         // This is for testing purposes
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("This is line " + (i + 1));
+        }
     }
 }
