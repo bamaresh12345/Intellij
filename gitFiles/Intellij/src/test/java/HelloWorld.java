ae69a4ef4d5fe227c25398bed55f817ef09ba043
bamaresh12345 <bamaresh2012@gmail.com>
2025-06-21 13:50:39 -0400

    feature11

--- gitFiles/Intellij/src/test/java/HelloWorld.java
+++ gitFiles/Intellij/src/test/java/HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Tesing Hello, World!");
 
     }
 
     public static void printMessage2() {
-        System.out.println("This is a message from HelloWorld class.");
+        System.out.println("test This is a message from HelloWorld class.");
     }
 }
