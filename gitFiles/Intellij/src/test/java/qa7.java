0439bba96093a700f6a5b137bd0e2d59277b344a
bamaresh12345 <bamaresh2012@gmail.com>
2025-06-21 23:34:31 -0400

    file7

---
+++ gitFiles/Intellij/src/test/java/qa7.java
@@ -0,0 +1,10 @@
+public class qa7 {
+    public static void main(String[] args) {
+        System.out.println("in qa7.java");
+
+        // This is a test for the qa7 class
+        for (int i = 0; i < 5; i++) {
+            System.out.println("This is line " + (i + 1));
+        }
+    }
+}
