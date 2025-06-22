6ed3d254cd716e25a8b4d934aba5a7eb3c34ee9b
bamaresh12345 <bamaresh2012@gmail.com>
2025-06-21 23:31:39 -0400

    file6

---
+++ gitFiles/Intellij/src/test/java/file6.java
@@ -0,0 +1,14 @@
+public class file6 {
+
+    public static void main(String[] args) {
+        System.out.println("in file6.java");
+
+        // This is a simple test to print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+            System.out.println("This is number: " + i);
+        }
+
+        // Additional functionality can be added here
+        System.out.println("End of file6.java");
+    }
+}
